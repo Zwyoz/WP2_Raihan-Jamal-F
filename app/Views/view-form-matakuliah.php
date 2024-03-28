9272787fb589fdf2bc7282467b2f26f7721d17b9
Zwyoz <raihanjamal111@gmail.com>
2024-03-28 10:07:59 +0700

    first commit

---
+++ app/Views/view-form-matakuliah.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Matakuliah</title>
+</head>
+<body>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form input Data mata kuliah
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="kode" id="kode">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama MTK</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="kode" id="kode">
+                    </td>
+                </tr>
+            </table>
+
+        </form>
+    </center>
+</body>
+</html>
